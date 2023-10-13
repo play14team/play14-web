a0f0c497d94521f0b6653128cbc8ec83ca98fbb3
Cédric PONTET <cpontet@agilepartner.net>
2023-10-14 00:08:51 +0200

    improved layout

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,37 +1,47 @@
 'use client'
 
-import { AppShell } from '@mantine/core';
+import { AppShell, Burger, Group } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import Header from './Header';
 import Navbar from './Navbar';
+import { ColorSchemeToggle } from './ColorSchemeToggle';
+import Image from 'next/image';
+import Play14Logo from '/public/logo/play14_black_bg_transparent_500x150.png';
+import UserAvatar from './UserAvatar';
 
 export default function Shell({ children, }: { children: React.ReactNode }) {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
 
   return (
-    <>
-      <AppShell
-        header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: 'sm',
-          collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-        }}
-        padding="md"
-      >
-        <AppShell.Header>
-          <Header />
-        </AppShell.Header>
-        <AppShell.Navbar p="md">
-          <Navbar />
-        </AppShell.Navbar>
-        <AppShell.Main>
-          Main
-          {children}
-        </AppShell.Main>
-      </AppShell>
-    </>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md" grow>
+          <Group>
+            <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+            <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+            <Image src={Play14Logo} width={166} height={50} alt="play14 logo" />
+          </Group>
+          <Group justify='flex-end'>
+            <ColorSchemeToggle />
+            <UserAvatar />
+          </Group>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        <Navbar />
+      </AppShell.Navbar>
+      <AppShell.Main>
+        {children}
+      </AppShell.Main>
 
+    </AppShell>
   )
 }
